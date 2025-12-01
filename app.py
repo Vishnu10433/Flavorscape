46e1ba47956106c77ccb44d321e160257fbd8840
Vishnu10433 <vishnukantvalmikcse2025@gmail.com>
2025-12-01 20:59:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'flavorscope@gmail.com'
-app.config['MAIL_PASSWORD'] = 'oiya zlhv irvc yowz'
+app.config['MAIL_PASSWORD'] = '              '
 
 mail = Mail(app)
 
